ef650dee263e28546789ce9d3366b7300528c142
DimaGrimach <chiki.briki2@gmail.com>
2023-06-30 14:07:56 +0300

    add setTimeout

--- main.js
+++ main.js
@@ -11,8 +11,9 @@
         }
     });
 })();
-/*Close adoric campaign*/
+
 (function () {
+    /*Close adoric campaign*/
     document.addEventListener('click', function (event) {
         const buttonClose = event.target.closest('.button.close-adoric');
         const textClose = event.target.closest('.element-text:not(.inImage)');
@@ -25,9 +26,7 @@
             buttonClose.style.setProperty('visibility', 'hidden');
         }
     });
-})();
 
-(function () {
     /*set winner code under scretch cart*/
     var DOC_KEY = '1N3bg7lhfpiQk8e8MLmNekjZTxJlhHXbEfdvC12Vrn-Y';
     var API_KEY = 'AIzaSyDB7Mk0_X06DqZHWlllSOont--5XAZ4tao';
@@ -59,23 +58,25 @@
             console.log('link-', link);
             /*select divs in part DOM our company,because in company we add unique name class in div with teg a,
              what we will change by new contents */
-            var allHrefOnBanner = document.querySelectorAll('.element-text:not(.inImage) A');
-            var spanTextOnBanner = document.querySelector('.element-text:not(.inImage) span');
+            const allHrefOnBanner = document.querySelectorAll('.element-text:not(.inImage) A');
+            const spanTextOnBanner = document.querySelector('.element-text:not(.inImage) span');
             /*approve present div in DOM adn change links*/
             allHrefOnBanner.forEach(function (oneHref) {
                 oneHref.href = link;
             });
 
             /*if present spliter we add teg <b> in first part text description*/
-            var indexSeparator = description.indexOf(":");
-            if (indexSeparator !== -1) {
-                var firstPart = description.slice(0, indexSeparator + 1);
-                var otherText = description.slice(indexSeparator + 1);
-                spanTextOnBanner.innerHTML = "<b>" + firstPart + "</b>" + otherText;
-            } else {
-                spanTextOnBanner.textContent = description;
-            }
-
+            const indexSeparator = description.indexOf(":");
+            setTimeout(function () {
+                if (indexSeparator !== -1) {
+                    const firstPart = description.slice(0, indexSeparator + 1);
+                    const otherText = description.slice(indexSeparator + 1);
+                    console.log('spanTextOnBanner-', spanTextOnBanner);
+                    spanTextOnBanner.innerHTML = "<b>" + firstPart + "</b>" + otherText;
+                } else {
+                    spanTextOnBanner.textContent = description;
+                }
+            }, 2000)
         }
     }
 
